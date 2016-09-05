d26485fdc3ef5e2f5c1c1bc6ea98b4da1ea7101a
V.Fisenko <vyacheslav.fisenko@gmail.com>
2016-09-05 17:33:09 +0300

    Project add/del - OK

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
 
-  before_action :signed_in_manager, only: [:index]
+  before_action :signed_in_manager
 
   def index
     @projects = current_manager.projects.order('priority')
@@ -13,6 +13,15 @@ class ProjectsController < ApplicationController
       format.html { redirect_to projects_path }
       format.js
     end
+  end
+
+  def destroy
+    @project = Project.destroy(params[:id])
+    respond_to do |format|
+      format.html { redirect_to projects_path }
+      format.js
+    end
+
   end
 
   private
